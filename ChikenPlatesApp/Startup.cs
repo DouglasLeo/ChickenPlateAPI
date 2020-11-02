9c12d31e04097c659bc8c7586108d6a8679c8dae
DouglasLeo <douglas.leonardo.05@gmail.com>
2020-11-01 23:59:35 -0300

    chore: configure DB connection

--- ChikenPlatesApp/Startup.cs
+++ ChikenPlatesApp/Startup.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChikenPlatesApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ChikenPlatesApp
 {
@@ -25,6 +28,9 @@ namespace ChikenPlatesApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ChickenContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("default"), builder =>
+                    builder.MigrationsAssembly("ChickenPlatesAPI")));
             services.AddControllers();
         }
 
